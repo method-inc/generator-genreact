4500b144a8d32d347a1d3546e38472ee13522859
Dustan Kasten <dustan.kasten@gmail.com>
2015-04-27 16:32:15 -0400

    suit validator: colored output for clarity

--- app/templates/scripts/_suit-conformance.js
+++ app/templates/scripts/_suit-conformance.js
@@ -4,6 +4,7 @@ var conformance = require('rework-suit-conformance');
 var glob = require('glob');
 var fs = require('fs');
 var path = require('path');
+var chalk = require('chalk');
 
 var globPath = path.join(
   __dirname,
@@ -13,17 +14,42 @@ var globPath = path.join(
   '*.css'
 );
 
+var loggedNamingConventions = false;
+var NAMING_CONVENTIONS_RE = /\ Please(.)*\./;
+
 glob(globPath, function(err, files) {
   files.forEach(function (file) {
     var css = fs.readFileSync(file, 'utf-8');
     try {
       rework(css).use(conformance);
     } catch (e) {
-      console.error('Error in ' + file.replace(
-        path.join(__dirname, '..'),
-        '.'
-      ));
-      console.error(e.message);
+      var message = e.message;
+      var matches = message.match(NAMING_CONVENTIONS_RE);
+      if (matches) {
+        message = message.replace(NAMING_CONVENTIONS_RE, '');
+      }
+
+      if (!loggedNamingConventions) {
+        loggedNamingConventions = true;
+        console.log(matches[0].trim());
+      }
+
+      console.error(
+        chalk.red('Error') + ' in ' +
+        chalk.magenta(
+          file.replace(
+            path.join(__dirname, '..'),
+            '.'
+          )
+        )
+      );
+
+      console.error(
+        message
+          .replace(/".*?"/, function(match) {
+            return '"' + chalk.yellow(match.replace(/"/g, '')) + '"';
+          })
+      );
     }
   });
 });
