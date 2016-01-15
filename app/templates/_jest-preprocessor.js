1beae80b73087aa27e2c15c13547c289a6e38b29
David Becher <david@skookum.com>
2016-01-15 13:48:41 -0500

    remove babel-jest and update regex

--- app/templates/_jest-preprocessor.js
+++ app/templates/_jest-preprocessor.js
@@ -1,9 +1,22 @@
-var babelJest = require('babel-jest');
+var babel = require('babel-core');
 
 module.exports = {
-  process: function(src, filename) {
-    process.env.BABEL_JEST_STAGE = 0;
-    return babelJest.process(src, filename)
-      .replace(/^require.*\.css.*;$/gm, '');
-  },
+  process: function (src, filename) {
+    var stage = 0;
+
+    // Ignore all files within node_modules
+    // babel files can be .js, .es, .jsx or .es6
+    if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
+      return babel.transform(
+        src.toString().replace(/((var|let|const)[\s]*[a-zA-Z0-9\-\_]+[\s]*=[\s]*)?require\('[a-zA-Z0-9\.\/\-\_]+\.s?css\'\);?/gm, ''),
+        { filename: filename,
+          stage: stage,
+          retainLines: true,
+          auxiliaryCommentBefore: "istanbul ignore next"
+        }
+      ).code;
+    }
+
+    return src;
+  }
 };
